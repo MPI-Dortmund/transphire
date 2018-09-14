73a7fd37302925b9615ae5b437867044e49fc91a
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2018-09-14 19:11:17 +0200

    Fix Distribution not Found behaviour

--- transphire/__init__.py
+++ transphire/__init__.py
@@ -11,6 +11,11 @@ try:
         # not installed, but there is another version that *is*
         raise pkg_resources.DistributionNotFound
 except pkg_resources.DistributionNotFound:
-    __version__ = 'Please install this project with setup.py'
+    try:
+        pip_output = os.popen('{0}/../../../../bin/pip freeze'.format(os.path.dirname(here))).read()
+        transphire = re.search(r'(transphire==[0-9\.]*)', pip_output)
+        __version__ = transphire.group(1).split('==')[1]
+    except:
+        __version__ = 'Please install this project with setup.py'
 else:
     __version__ = _dist.version
