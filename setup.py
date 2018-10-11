d819c19b02b2ce11606e5337b4bcfd8f4e843aae
em-transfer-user <em-transfer-user@em-transfer-2.mpi-dortmund.mpg.de>
2018-10-11 12:53:54 +0200

    Added transphire_transform as a dependency

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setuptools.setup(
         'cython>=0.28.0,<0.29.0',
         'telepot>=12.0,<13.0',
         'imageio>=2.3.0,<2.4.0',
+        'transphire_transform>=0.0.1',
         ],
     classifiers = (
         'Programming Language :: Python :: 3.6',
