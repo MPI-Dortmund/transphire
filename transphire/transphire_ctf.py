f14a22f4210f590f12c99c4dc47550643da21b09
em-transfer-user <em-transfer-user@em-transfer-3.client.mpi-dortmund.mpg.de>
2018-12-03 20:25:49 +0100

    Fix superresolution images

--- transphire/transphire_ctf.py
+++ transphire/transphire_ctf.py
@@ -867,7 +867,10 @@ def create_jpg_file(input_mrc_file, settings, ctf_name):
         input_data = tu.normalize_image(input_data)
 
         if input_data.shape[0] > 512:
-            original_shape = 4096
+            if input_data.shape[0] > 4096:
+                original_shape = 4096*2
+            else:
+                original_shape = 4096
             bin_shape = 512
             ratio = original_shape / bin_shape
             pad_x = original_shape - input_data.shape[0]
