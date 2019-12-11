1139057b798858902ebc6230b75088c4d993f825
em-transfer-user <em-transfer-user@em-transfer-3.client.mpi-dortmund.mpg.de>
2019-12-11 18:08:26 +0100

    Remove always stop flag

--- transphire/processworker.py
+++ transphire/processworker.py
@@ -715,7 +715,6 @@ class ProcessWorker(QObject):
         except FileNotFoundError:
             pass
 
-        self.stop = True
         self.settings['do_feedback_loop'] = mp.Value('i', self.settings['do_feedback_loop'])
         if self.settings['do_feedback_loop'].value == 0:
             queue_com['status'].put([
