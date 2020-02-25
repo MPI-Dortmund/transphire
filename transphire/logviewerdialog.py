d29988539a8de0660a1c265a9dc489bc5533e953
em-transfer-user <em-transfer-user@em-transfer-3.client.mpi-dortmund.mpg.de>
2020-02-25 16:23:21 +0100

    Add logviewerdialog

---
+++ transphire/logviewerdialog.py
@@ -0,0 +1,30 @@
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QDialog
+from transphire import tabdocker
+
+
+class LogViewerDialog(QDialog):
+
+
+    def __init__(self, parent=None):
+        super(LogViewerDialog, self).__init__(parent)
+
+        central_raw_layout = QVBoxLayout(self)
+        central_raw_layout.setContentsMargins(0, 0, 0, 0)
+        central_widget_raw = QWidget(self)
+        central_widget_raw.setObjectName('central_raw')
+        central_raw_layout.addWidget(central_widget_raw)
+
+        central_layout = QVBoxLayout(central_widget_raw)
+        central_widget = QWidget(self)
+        central_widget.setObjectName('central')
+        central_layout.addWidget(central_widget)
+
+        layout = QVBoxLayout(central_widget)
+
+        self.tabs = tabdocker.TabDocker(self)
+        self.tabs.setObjectName('tab')
+
+        layout.addWidget(self.tabs)
+
+    def add_tab(self, widget, name):
+        self.tabs.add_tab(widget, name)
